2bce8e78ab13c7709e5bc8b42741ce6585b55464
monaja <frmonaja95@gmail.com>
2018-10-20 04:23:27 +0300

    created the first project for exercise 1

---
+++ exercise1/MonajaFrancis/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mfo-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
